767f930ad5ff933aed0dc678efab5ec12137159a
bruce2233 <864427717@qq.com>
2022-07-20 14:17:40 +0800

    socket test

--- go-learning/netpoll/epoll_test.go
+++ go-learning/netpoll/epoll_test.go
@@ -39,30 +39,27 @@ func TestTcpSocket(t *testing.T) {
 	t.Log(fd)
 	t.Log(netAddr)
 	// net.Listener.Accept()
+	time.Sleep(1e12)
 
 }
 
 func TestEpollListener(t *testing.T) {
-	socketFd, netAddr, err := tcpSocket("tcp", "192.168.0.1:8866", false)
+	socketFd, netAddr, err := tcpSocket("tcp", "127.0.0.1:8866", false)
 	if err != nil {
-		t.Log("sopcket error")
+		t.Log("socket error")
 	}
 	t.Log(socketFd)
 	t.Log(netAddr)
 
 	epollFd, err := unix.EpollCreate1(unix.EPOLL_CLOEXEC)
 	if err != nil {
+		t.Log(err)
 	}
 	t.Log(epollFd)
 
 	unix.EpollCtl(epollFd, unix.EPOLL_CTL_ADD, socketFd, &unix.EpollEvent{
 		Fd: int32(socketFd), Events: unix.EPOLLIN})
 	el := make([]unix.EpollEvent, 128)
-	err = unix.Listen(socketFd, 0)
-	t.Log("after listen")
-	if err != nil {
-		t.Log(err)
-	}
 	n, err := unix.EpollWait(epollFd, el, -1)
 	t.Log(el)
 	t.Log(err)
@@ -111,6 +108,11 @@ func TestWrite(t *testing.T) {
 	if err != nil {
 		t.Log(err)
 	}
+	epfd, err :=unix.EpollCreate1(1)
+	if err!=nil{
+		t.Log(err)
+	}
+	unix.
 	unix.Write(fd, []byte("Hello IO"))
 }
 
@@ -123,7 +125,26 @@ func TestWriteSocket(t *testing.T) {
 }
 
 func TestSocketCall(t *testing.T) {
-	unix.Socket(unix.AF_INET, unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC|unix.SOCK_STREAM, unix.IPPROTO_TCP)
+	sa, family, tcpAddr, ipv6only, err := GetTCPSockAddr("tcp", "127.0.0.1:8866")
+	if err != nil {
+		t.Log(err)
+	}
+
+	t.Log(sa)
+	t.Log(family)
+	t.Log(tcpAddr)
+	t.Log(ipv6only)
+
+	fd, err := unix.Socket(family, unix.SOCK_STREAM|unix.SOCK_NONBLOCK|unix.SOCK_CLOEXEC, unix.IPPROTO_TCP)
+
+	t.Log(fd)
+	if err != nil {
+		t.Log(err)
+	}
+	unix.Bind(fd, sa)
+	unix.Listen(fd, 1024)
+	t.Log("after listen")
+	time.Sleep(1e12)
 }
 
 func TestGetTCPAddr(t *testing.T) {
